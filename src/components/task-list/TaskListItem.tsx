152cc1055151d603e1d2c861862b7609a66a09b1
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-06-08 17:54:31 -0500

    Added a delete action.

--- src/components/task-list/TaskListItem.tsx
+++ src/components/task-list/TaskListItem.tsx
@@ -1,27 +1,35 @@
 'use client';
 
 import clsx from "clsx";
-import { useState } from "react";
+import { deleteTaskById } from "@/actions/delete-task-by-id";
+import { Task } from "@/interfaces/task.interface";
 import { FaPen, FaTrash } from "react-icons/fa";
 
 interface Props {
-  content: string;
+  task: Task;
 }
 
-export const TaskListItem = ({ content }: Props) => {
-  const [check, setCheck] = useState(false);
+export const TaskListItem = ({ task }: Props) => {
+  
+  const onDelete = async () => {
+    await deleteTaskById(task.id);
+  };
+
   return (
     <div className="flex items-center gap-2 border rounded-lg p-3">
-      <input type="checkbox" name="" id="" className="scale-150 m-3" checked={check} onChange={(event) => setCheck(event.target.checked)} />
+      <input type="checkbox" name="" id="" className="scale-150 m-3" checked={task.completed} />
       <p className={clsx(
         'flex-1',
-        { 'text-gray-400 line-through italic': check }
-      )}>{content}</p>
+        { 'text-gray-400 line-through italic': task.completed }
+      )}>{task.content}</p>
       <div className="flex gap-3">
         <button className="border rounded-full text-gray-600 bg-gray-200 hover:bg-blue-500 hover:text-white p-2 transition-all">
           <FaPen />
         </button>
-        <button className="border rounded-full text-gray-600 bg-gray-200 hover:bg-red-500 hover:text-white p-2 transition-all">
+        <button
+          className="border rounded-full text-gray-600 bg-gray-200 hover:bg-red-500 hover:text-white p-2 transition-all"
+          onClick={onDelete}
+        >
           <FaTrash />
         </button>
       </div>
