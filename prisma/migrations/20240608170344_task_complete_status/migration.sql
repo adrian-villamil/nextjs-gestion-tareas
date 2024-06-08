5daea0a45f516f9c78459dd3e0cdd9aa7dfca335
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-06-08 12:06:20 -0500

    Updated task model - added complete status.

---
+++ prisma/migrations/20240608170344_task_complete_status/migration.sql
@@ -0,0 +1,13 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Task" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "content" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false
+);
+INSERT INTO "new_Task" ("content", "id") SELECT "content", "id" FROM "Task";
+DROP TABLE "Task";
+ALTER TABLE "new_Task" RENAME TO "Task";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
