d5ed2ee9a7e7ea53cf0fe10e044b694bdfd61a9b
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-06-09 13:33:18 -0500

    Updated home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+import { getAllTasks } from "@/actions";
 import { TaskForm, TaskList } from "@/components";
 
-export default function Home() {
+export default async function Home() {
+  const tasks = await getAllTasks();
+
   return (
     <main>
-      <div className="flex flex-col gap-8 w-[680px] min-h-[500px] mt-20 mx-auto p-5">
-      <h1 className="text-center text-4xl font-semibold">Task Managment</h1>
+      <div className="flex flex-col gap-12 w-full sm:max-w-screen-sm mt-20 mx-auto p-5">
+        <h1 className="text-center text-4xl font-semibold">Task Managment</h1>
         <TaskForm />
-        <TaskList tasks={[]} />
+        <TaskList tasks={tasks} />
       </div>
     </main>
   );
